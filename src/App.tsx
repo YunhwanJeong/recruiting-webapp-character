7877793a7fa493094e3549bb761ed7eb7462e224
yunhwan <jimmy53120488@gmail.com>
2024-11-14 14:07:10 -0500

    feat: add reducer with initial state

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import { useState } from "react";
+import { useReducer } from "react";
 import "./App.css";
+import { ATTRIBUTE_LIST } from "./consts";
 
 type Action =
   | { type: "INCREMENT"; attribute: string }
@@ -9,21 +10,34 @@ type Attributes = {
   [key: string]: number;
 };
 
+const initialAttributes: Attributes = ATTRIBUTE_LIST.reduce(
+  (acc, attr) => ({ ...acc, [attr]: 10 }),
+  {}
+);
+
+const reducer = (state: Attributes, action: Action): Attributes => {
+  switch (action.type) {
+    case "INCREMENT":
+      return { ...state, [action.attribute]: state[action.attribute] + 1 };
+    case "DECREMENT":
+      return {
+        ...state,
+        [action.attribute]: Math.max(0, state[action.attribute] - 1),
+      };
+    default:
+      return state;
+  }
+};
+
 function App() {
-  const [num, setNum] = useState<number>(0);
+  const [attributes, dispatch] = useReducer(reducer, initialAttributes);
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>React Coding Exercise</h1>
       </header>
-      <section className="App-section">
-        <div>
-          Value:
-          {num}
-          <button>+</button>
-          <button>-</button>
-        </div>
-      </section>
+      <section className="App-section"></section>
     </div>
   );
 }
